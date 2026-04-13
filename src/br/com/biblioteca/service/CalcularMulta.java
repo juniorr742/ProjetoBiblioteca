71d0c6163428c51afa31cc434e5d128dc0974026
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-04-13 11:09:26 -0300

    "feat: criando classe de cálculo de multa"

---
+++ src/br/com/biblioteca/service/CalcularMulta.java
@@ -0,0 +1,14 @@
+package br.com.biblioteca.service;
+
+public class CalcularMulta {
+    public static final int PRAZO_PADRAO = 7;
+    public static final double VALOR_MULTA_DIARIA = 2.0;
+
+    public double valorCalculado(int diasCorridos){
+        if (diasCorridos < PRAZO_PADRAO){
+            return 0.0;
+        }
+        double valor = (diasCorridos - PRAZO_PADRAO) * VALOR_MULTA_DIARIA;
+        return valor;
+    }
+}
